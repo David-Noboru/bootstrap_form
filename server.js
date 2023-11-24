5774dc2b7691067b1729e3d2d007ab5175e217e2
David-Noboru <david.noboru07@aluno.ifce.edu.br>
2023-11-24 14:49:34 -0300

    vamos que vamos

--- server.js
+++ server.js
@@ -4,17 +4,18 @@ const { link } = require('fs')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Muitos bem-vindos!')
+  res.send('Menu principal.<br> Muito bem-vindos! <a href="/agora">Ir para agora deu certo</a>')
 })
 
 app.listen(3000, () => {
   console.log('Server online')
 })
 
+
 app.get('/agora', (req, res) => {
-    res.send('Agora deu certo!')
+    res.send('Agora deu certo! <br><a href="/">Voltar pro Home!</a> <br> <a href="/eita"> Estamos linkando</a>')
   })
 
-  app.get('/eita', (req, res) => {
-    res.send('Agora d')
+app.get('/eita', (req, res) => {
+    res.send('Estamos Linkando <a href="/"> Voltando para o Home</a> <br> <a href="/agora"> Voltando para agora deu certo</a>')
   })
